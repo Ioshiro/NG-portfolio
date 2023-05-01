8df961022b4ae20aef2e966283831f2f8d39fc0d
ioshiro <dmitry.golyshev.dev@gmail.com>
2023-05-01 18:26:04 +0200

    add responsive side nav menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import {Breakpoints, BreakpointObserver} from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-portfolio';  
+export class AppComponent implements OnInit  {
+  title = 'ng-portfolio';
+  hideSideMenu = true;  
   navLinks: any[];
   activeLinkIndex = -1; 
-  constructor(private router: Router) {
+  constructor(private router: Router,  private responsive: BreakpointObserver) {
     this.navLinks = [
         {
             label: 'Home',
@@ -39,5 +42,31 @@ ngOnInit(): void {
   this.router.events.subscribe((res) => {
       this.activeLinkIndex = this.navLinks.indexOf(this.navLinks.find(tab => tab.link === '.' + this.router.url));
   });
+
+  this.responsive.observe([
+    Breakpoints.HandsetPortrait, 
+    Breakpoints.HandsetLandscape,
+    Breakpoints.TabletLandscape,     
+    Breakpoints.WebLandscape,
+    ])
+    .subscribe(result => {
+
+      const breakpoints = result.breakpoints;
+
+      if (breakpoints[Breakpoints.HandsetPortrait] || breakpoints[Breakpoints.HandsetLandscape]) {
+        console.log("screens matches TabletPortrait");
+        this.hideSideMenu = false;
+      }
+      else if (breakpoints[Breakpoints.WebLandscape] || breakpoints[Breakpoints.TabletLandscape]) {
+        console.log("screens matches HandsetLandscape");
+        this.hideSideMenu = true;
+      }
+  });
+}
+toggleSidenav(sidenav: MatSidenav) {
+  sidenav.toggle();
+}
+closeSidenav(sidenav: MatSidenav) {
+  sidenav.close();
 }
 }
